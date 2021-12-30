1add222670eb5cf8157e4654492b91175e988479
Kethsar <kethstuff@gmail.com>
2021-12-29 19:42:10 -0800

    Add sha256 checksum to build and upload steps

--- make.sh
+++ make.sh
@@ -5,3 +5,5 @@ GOOS=windows GOARCH=amd64 go build -ldflags "-X main.Commit=-$(git rev-parse --s
 
 zip ytarchive_linux_amd64.zip ytarchive
 zip ytarchive_windows_amd64.zip ytarchive.exe
+
+sha256sum ytarchive_linux_amd64.zip ytarchive_windows_amd64.zip > SHA2-256SUMS
