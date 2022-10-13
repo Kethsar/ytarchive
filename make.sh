1a4bc71d734ee2b890ff675d792f3c51c0a5a934
Kethsar <kethstuff@gmail.com>
2022-10-12 22:38:36 -0700

    Update make script and github release workflow
    Should create a new full release when a semver tag is created
    Should hopefully ignore the dev branch when doing this

--- make.sh
+++ make.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 export CGO_ENABLED=0
-go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
-GOOS=windows GOARCH=amd64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
+
+if [[ -n "$1" ]]; then
+    go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
+    GOOS=windows GOARCH=amd64 go build -ldflags "-X main.Commit=-$(git rev-parse --short HEAD)"
+else
+    go build
+    GOOS=windows GOARCH=amd64 go build
+fi
 
 zip ytarchive_linux_amd64.zip ytarchive
 zip ytarchive_windows_amd64.zip ytarchive.exe
