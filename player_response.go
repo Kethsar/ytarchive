156f0238c90256e2f6e557c9466833c7d5698289
Kethsar <kethstuff@gmail.com>
2024-02-01 13:56:31 -0800

    Properly set LiveURL state value after finding a stream
    Should fix #171

--- player_response.go
+++ player_response.go
@@ -589,7 +589,7 @@ func (di *DownloadInfo) GetPlayablePlayerResponse() (retrieved int, pr *PlayerRe
 			if isLiveURL {
 				di.URL = fmt.Sprintf("https://www.youtube.com/watch?v=%s", di.VideoID)
 				di.MembersOnly = false
-				isLiveURL = false
+				di.LiveURL = false
 				continue
 			}
 
